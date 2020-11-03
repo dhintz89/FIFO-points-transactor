61b3b238aa75111622798219d51c5ee3a18f35d2
dhintz <dhintz89@gmail.com>
2020-11-03 18:36:49 -0500

    Add functionality for Transaction Endpoints: points_balance, add_points, show transaction, and delete transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,4 +6,46 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
+  # index
+  def points_balance
+    user = User.find(params[:user_id])
+    transactions = user.transactions.all.sort_by{ |obj| obj.created_at }
+    render json: transactions.to_json
+  end
+
+  # create
+  def add_points
+    user = User.find(params[:user_id])
+    if transaction_params[:points] < 0
+      # remove points
+    else
+      transaction = user.transactions.new(transaction_params)
+      if transaction.valid?
+        transaction.save
+        render json: transaction.to_json
+      else
+        render json: {errors: transaction.errors}, status: :bad_request
+      end
+    end
+  end
+
+  def deduct_points
+
+  end
+
+  def destroy
+    transaction = Transaction.find(params[:id])
+    transaction.destroy
+    render json: transaction.to_json
+  end
+
+  def show
+    render json: Transaction.find(params[:id]).to_json
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:payer_name, :points)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-
+  alias_attribute :recipient_id, :user_id
 
 
 end
